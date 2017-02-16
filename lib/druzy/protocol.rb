445bc22d7164a6607421714e5f181ef3151e7997
druzy <jonathan.legreneur@free.fr>
2017-02-16 03:01:07 +0100

    remove unused methods

--- lib/druzy/protocol.rb
+++ lib/druzy/protocol.rb
@@ -3,34 +3,22 @@ require_relative 'protocol/device'
 
 module Druzy
   module Protocol
-    
+
     class Discoverer
       @@discoverers=[]
       @@plugin_dir=File.dirname(__FILE__)+'/protocol/plugin/'
       #@@plugin_dir="/usr/lib/ruby/vendor_ruby/druzy"+"/protocol/plugin/"
-      
+
       def self.get_discoverers
         Dir[File.join(@@plugin_dir, '*.rb')].each {|file| require "druzy/protocol/plugin/"+File.basename(file,"*.rb")}
-        return @@discoverers         
+        return @@discoverers
       end
-      
+
       def self.inherited(klass)
         @@discoverers << klass.new
       end
-      
-      def get_ports
-        return []
-      end
-      
-      def start_discoverer(delay=10, identifier=nil)
-      end
-      
-      def stop_discoverer
-      end
-      
-      def restart_discoverer
-      end
+
     end
- 
+
   end
 end
